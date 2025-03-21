3c6b044fa74168ec33d01c633119296f8bbff5bb
SaraSLeite <saraleite2708@gmail.com>
2025-03-21 10:18:52 -0300

    adicionando api para validação de cpf e cnpj. Transformando a aplicação em C#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NomeDoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo!");
+        }
+    }
+}
